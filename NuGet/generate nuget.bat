12b274cb686514c2aaf029a7adb28565d229c8e7
Jorn Filho <jornfilho@gmail.com>
2014-05-02 04:54:10 -0300

    Nuget files adjusts

--- NuGet/generate nuget.bat
+++ NuGet/generate nuget.bat
@@ -1,8 +1,6 @@
-C:\
-F:\
+C:
+F:
 cd .net-Dev-Utils
 cd NuGet
 nuget pack ../DevUtils/DevUtils/DevUtils.csproj -IncludeReferencedProjects -Prop Configuration=Release
-timeout 10
-nuget push DevUtils.1.0.0.0.nupkg
 pause
---
+++ NuGet/publish nuget.bat
@@ -0,0 +1,6 @@
+C:\
+F:\
+cd .net-Dev-Utils
+cd NuGet
+nuget push DevUtils.1.0.0.1.nupkg
+pause
