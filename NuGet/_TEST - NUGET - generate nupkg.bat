e4763fd57ff9a5627f05fced502ec5f5cec15994
Jorn Filho <jornfilho@gmail.com>
2014-05-06 00:25:47 -0300

    Adjusts for nuget multiple framework

---
+++ NuGet/_TEST - NUGET - generate nupkg.bat
@@ -0,0 +1,6 @@
+C:
+F:
+cd .net-Dev-Utils
+cd NuGet
+nuget pack ../DevUtils/DevUtils/DevUtils.csproj-teste.nuspec
+pause
---
+++ NuGet/_TEST - NUGET - publish.bat
@@ -0,0 +1,6 @@
+C:\
+F:\
+cd .net-Dev-Utils
+cd NuGet
+nuget push DevUtils_publish-test.1.0.3.1.nupkg
+pause
