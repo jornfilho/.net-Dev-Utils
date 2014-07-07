2cf30881c7232784aaa70e50d159360f1f7c015b
Michel Henrique Aquino Santos <michel.has@gmail.com>
2014-07-06 21:32:41 -0300

    Adding ObjectExtension class. IsNull method.

---
+++ DevUtils.test/DevUtils.test/ObjectExtensions/ObjectExtension.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using DevUtils.ObjectExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevUtils.test.ObjectExtensions
+{
+    [TestClass]
+    public class ObjectExtension
+    {
+        [TestMethod]
+        public void TestVerifyObjectNull()
+        {
+            Object objectDefaultNull = null;
+            Object objectDefaultNotNull = new Object();
+            Foo objectFooNull = null;
+            Foo objectFooNotNull = new Foo();
+            Foo objectFooWithNameNotNull = new Foo { Name = "test" };
+            EnumTest? enumNull = null;
+            EnumTest? enumNotNull = EnumTest.First;
+            int? variableIntNull = null;
+            int variableIntNotNull = 10;
+            string stringNull = null;
+            string stringNotNull = string.Empty;
+            bool? booleanNull = null;
+            bool? booleanNotNull = false;
+            byte? byteNull = null;
+            byte? byteNotNull = 1;
+            decimal? decimalNull = null;
+            decimal? decimalNotNull = 1;
+            double? doubleNull = null;
+            double? doubleNotNull = 1;
+            float? floatNull = null;
+            float? floatNotNull = 1;
+            long? longNull = null;
+            long? longNotNull = 1;
+            short? shortNull = null;
+            short? shortNotNull = 1;
+            
+            Assert.IsTrue(objectDefaultNull.IsNull());
+            Assert.IsFalse(objectDefaultNotNull.IsNull());
+            Assert.IsTrue(objectFooNull.IsNull());
+            Assert.IsFalse(objectFooNotNull.IsNull());
+            Assert.IsFalse(objectFooWithNameNotNull.IsNull());
+            Assert.IsTrue(enumNull.IsNull());
+            Assert.IsFalse(enumNotNull.IsNull());
+            Assert.IsTrue(variableIntNull.IsNull());
+            Assert.IsFalse(variableIntNotNull.IsNull());
+            Assert.IsTrue(stringNull.IsNull());
+            Assert.IsFalse(stringNotNull.IsNull());
+            Assert.IsTrue(booleanNull.IsNull());
+            Assert.IsFalse(booleanNotNull.IsNull());
+            Assert.IsTrue(byteNull.IsNull());
+            Assert.IsFalse(byteNotNull.IsNull());
+            Assert.IsTrue(decimalNull.IsNull());
+            Assert.IsFalse(decimalNotNull.IsNull());
+            Assert.IsTrue(doubleNull.IsNull());
+            Assert.IsFalse(doubleNotNull.IsNull());
+            Assert.IsTrue(floatNull.IsNull());
+            Assert.IsFalse(floatNotNull.IsNull());
+            Assert.IsTrue(longNull.IsNull());
+            Assert.IsFalse(longNotNull.IsNull());
+            Assert.IsTrue(shortNull.IsNull());
+            Assert.IsFalse(shortNotNull.IsNull());
+        }
+    }
+
+    /// <summary>
+    ///     Class for test
+    /// </summary>
+    internal class Foo
+    {
+        public string Name { get; set; }
+    }
+
+    /// <summary>
+    ///     Enum for test
+    /// </summary>
+    internal enum EnumTest
+    {
+        First = 1
+    }
+}
---
+++ DevUtils/DevUtils/ObjectExtensions/ObjectExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace DevUtils.ObjectExtensions
+{
+    /// <summary>
+    ///     Object extension
+    /// </summary>
+    public static class ObjectExtension
+    {
+        /// <summary>
+        ///     Checks if an object is null
+        /// </summary>
+        /// <param name="obj"> Object to check </param>
+        /// <returns> Flag indicating whether the object is null </returns>
+        public static bool IsNull(this object obj)
+        {
+            return obj == null;
+        }
+    }
+}
